0a7acb47af8405b5d763a2057675e049a55e6ee7
mohamed14787 <mohamedelshaarawy87@gmail.com>
2025-05-04 11:08:55 +0200

    Final

--- frontend/src/Components/RightDetails.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CoverageBox from "./CoverageBox";
-export default function RightDetails() {
-  return (
-    <div className="right-details">
-      <CoverageBox />
-    </div>
-  );
-}
